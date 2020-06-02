6603df2c00bfe35ec8907548c815ce4a18e26773
kentaro-hayashi <k@8-3.cc>
2020-06-02 23:31:48 +0900

    removed unusing components

--- pages/index.vue
+++ pages/index.vue
@@ -22,14 +22,8 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
-  },
+  components: {},
   data() {
     return {}
   },
