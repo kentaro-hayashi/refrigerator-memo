5ec87e481c6a0ab16e323fe063f0fddb4f063e54
kentaro-hayashi <k@8-3.cc>
2020-06-07 17:04:06 +0900

    style: modified default style

--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,19 @@
 <template lang="pug">
-  firebase-auth
+  v-container
+    v-row(no-gutters justify="center")
+      v-col(cols="12" sm="8" md="6")
+        v-card
+          .title
+            v-icon local_florist
+            | これは何か？
+          p 冷蔵庫やキッチンに保管している食材を登録して、買い物時に何を買えばいいのか教えてくれるツールです
+          .title
+            v-icon local_florist
+            | 使い方は？
+          p Googleの認証でサインアップし、食材を登録します。
+            br
+            | 食材の在庫が一定の分量を下回った場合に買い物リストに載るようになっています。
+          firebase-auth
 </template>
 
 <script>
